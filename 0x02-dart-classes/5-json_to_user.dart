4cd66ea51800966f98cb08c183fe4383d33073c9
Andreanpa <andreanpa94@gmail.com>
2023-03-26 22:25:14 -0500

    Update

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,32 +1,37 @@
 class User {
-  late int id;
-  late String name;
-  late int? age;
-  late double? height;
+  int? id;
+  String? name;
+  int? age;
+  double? height;
 
-  User({required int id, required String name, int? age, double? height}) {
+  User({required int id, required String name,
+        required int age, required double height}) {
     this.id = id;
     this.name = name;
     this.age = age;
     this.height = height;
   }
 
-  Map toJson() => {
-        'id': this.id,
-        'name': this.name,
-        'age': this.age,
-        'height': this.height
-      };
-
-  static User fromJson(Map<dynamic, dynamic> userJson) {
-    return new User(
-        id: userJson['id'],
-        name: userJson['name'],
-        age: userJson['age'],
-        height: userJson['height']);
+  Map<String, Object?> toJson () {
+    return {
+      'id': this.id,
+      'name': this.name,
+      'age': this.age,
+      'height': this.height
+    };
   }
 
-  String toString() {
-    return 'User(id : ${this.id} ,name: ${this.name},  age: ${this.age}, height: ${this.height})';
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height']
+    );
+  }
+
+  @override
+  String toString () {
+    return "User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height})";
   }
 }
